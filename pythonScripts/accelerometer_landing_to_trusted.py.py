43c7627ec4dc53108830fce462c6bbe0daf369e3
Sarai Lopez <108087900+TestingSaGit@users.noreply.github.com>
2025-05-09 17:23:46 -0600

    Add files via upload
    Upload project files

---
+++ pythonScripts/accelerometer_landing_to_trusted.py.py
@@ -0,0 +1,51 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1746725847617 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1746725847617")
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1746725849528 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1746725849528")
+
+# Script generated for node Customer Privacy Filter
+CustomerPrivacyFilter_node1746725955188 = Join.apply(frame1=CustomerTrusted_node1746725849528, frame2=AccelerometerLanding_node1746725847617, keys1=["email"], keys2=["user"], transformation_ctx="CustomerPrivacyFilter_node1746725955188")
+
+# Script generated for node Drop Columns
+SqlQuery0 = '''
+select user,timeStamp,x,y,z from myDataSource
+
+'''
+DropColumns_node1746729486050 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerPrivacyFilter_node1746725955188}, transformation_ctx = "DropColumns_node1746729486050")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=DropColumns_node1746729486050, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1746725818992", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1746726108990 = glueContext.getSink(path="s3://udacity-stedi-lake/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1746726108990")
+AmazonS3_node1746726108990.setCatalogInfo(catalogDatabase="stedi",catalogTableName="accelerometer_trusted")
+AmazonS3_node1746726108990.setFormat("json")
+AmazonS3_node1746726108990.writeFrame(DropColumns_node1746729486050)
+job.commit()
---
+++ pythonScripts/step_trainer_landing_to_trusted.py
@@ -0,0 +1,48 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Customer Curated
+CustomerCurated_node1746725849528 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_curated", transformation_ctx="CustomerCurated_node1746725849528")
+
+# Script generated for node Step Trainer Landing
+StepTrainerLanding_node1746820767113 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_landing", transformation_ctx="StepTrainerLanding_node1746820767113")
+
+# Script generated for node Filter Step Trainer Trusted
+SqlQuery0 = '''
+select stl.sensorreadingtime,stl.serialnumber,stl.distancefromobject
+from cc join stl ON cc.serialnumber = stl.serialnumber
+'''
+FilterStepTrainerTrusted_node1746729486050 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"cc":CustomerCurated_node1746725849528, "stl":StepTrainerLanding_node1746820767113}, transformation_ctx = "FilterStepTrainerTrusted_node1746729486050")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=FilterStepTrainerTrusted_node1746729486050, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1746725818992", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1746726108990 = glueContext.getSink(path="s3://udacity-stedi-lake/step_trainer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1746726108990")
+AmazonS3_node1746726108990.setCatalogInfo(catalogDatabase="stedi",catalogTableName="step_trainer_trusted")
+AmazonS3_node1746726108990.setFormat("json")
+AmazonS3_node1746726108990.writeFrame(FilterStepTrainerTrusted_node1746729486050)
+job.commit()
